d24f5592b5c2c6cb35186ad3e708559f792abd25
Ethan Chester <43710677+ethan-chester@users.noreply.github.com>
2024-10-10 14:31:30 -0400

    Clean up code

--- exercise3/world/world.gd
+++ exercise3/world/world.gd
@@ -7,15 +7,9 @@ func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("launch"):
 		$ball.apply_impulse((Vector3.FORWARD + Vector3.UP).normalized() * power)
 
-	
-
-
 func _on_ball_body_entered(body: Node) -> void:
 	if body.is_in_group("targets"):
 		%WinLabel.text = "YOU WIN"
-		await get_tree().create_timer(2.0).timeout
-
-		get_tree().change_scene_to_file("res://main_menu.tscn")
-
-
 		
+		await get_tree().create_timer(2.0).timeout
+		get_tree().change_scene_to_file("res://main menu/main_menu.tscn")
